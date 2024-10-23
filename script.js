fcf929b73fb792501162588de0924cf1cb2a33b2
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-10-23 11:15:31 +0100

    update

--- script.js
+++ script.js
@@ -90,8 +90,3 @@ const allcontainer = gsap.utils.toArray(".container-item");
 			},
 			0.2
 		);
-        const mouse = document.querySelector('#mouse')
-        window.addEventListener('mousemove', (e) => {
-          mouse.style.top = e.pageY + 'px'
-          mouse.style.left = e.pageX + 'px'
-        })
